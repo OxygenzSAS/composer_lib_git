1f17dd52db8deaf493b2dc7f88d409e162b403e4
nvanhaezebrouck <113363985+nvanhaezebrouck@users.noreply.github.com>
2023-04-18 18:18:01 +0200

    Update Git.php

--- src/Git.php
+++ src/Git.php
@@ -8,10 +8,13 @@ class Git
     /** recuperation de la version git */
     public static function getVersion(){
         try{
+            ob_start();
             exec('git describe --always',$versionMiniHash);
             if(isset($versionMiniHash[0])){
                 $version = $versionMiniHash[0];
             }
+            @ob_end_clean();
+
         }catch(\Throwable $e){}
 
         return $version ?? '';
